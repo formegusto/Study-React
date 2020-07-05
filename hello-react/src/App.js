d38b71df49c0daac77431831781c877834b3afee
formegusto <formegusto@gmail.com>
2020-07-05 23:39:56 +0900

    React - (2) JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const name = "반갑다 리액태헌 리액트 생태계는 널 원한다";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 이게 주석 작성방법입니다. */}
+      <div
+        className="react" // 이 안에서도 할 수 있꼬
+      >
+        {name}
+      </div>
+      // 하지만 이렇게 난리치면서 하면 /* 화면에 다 보이는 주석을 쓰고 있는
+      겁니다 ㅎㅎ */
+      <input />
+    </>
   );
 }
 
