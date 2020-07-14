a4ba0a9f65ef4979d7ba9eed57cd0b332b7af56a
formegusto <formegusto@gmail.com>
2020-07-14 23:53:09 +0900

    React - TodosList END

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
     return (
         <div className="TodoList">
-            <TodoListItem/>
-            <TodoListItem/>
-            <TodoListItem/>
+            {todos.map(todo => (
+                <TodoListItem todo={todo} onRemove={onRemove} onToggle={onToggle} 
+                    key={todo.id}/>
+            ))}
         </div>
     );
 };
