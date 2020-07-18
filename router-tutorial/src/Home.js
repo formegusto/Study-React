0fc7bb22a9f2d5df3b93cd6f507e53f8e7c66016
formegusto <formegusto@gmail.com>
2020-07-18 13:13:39 +0900

    React - React Router

---
+++ router-tutorial/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>홈</h1>
+            <p>홈, 그 페이지는 먼저 보여지는 페이지. 오...</p>
+        </div>
+    );
+}
+
+export default Home;
